a308a967169d910db7a1a40c440f65173f7367f4
Jonathan <jonicgp97@gmail.com>
2020-09-18 11:17:30 -0500

    Move to external config for publish and make

---
+++ forge.config.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+module.exports = {
+  packagerConfig: {},
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      platforms: ['linux'],
+      config: {
+        repository: {
+          owner: 'jonathangomz',
+          name: 'notionlx'
+        },
+        prerelease: true,
+        draft: true,
+        authToken: process.env.GITHUB_TOKEN
+      }
+    }
+  ],
+  makers: [
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: [
+        'darwin'
+      ]
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {
+        options: {
+          name: 'NotionLX',
+          productName: 'NotionLX',
+          maintainer: 'Jonathan Gómez',
+          homepage: 'https://jonathangomz.github.io/notionlx',
+          icon: 'src/icons/notion-icon.png',
+          description: 'Simple Notion app for Linux',
+          productDescription: "This is an unofficial and open source app to use Notion on Linux. It's in early version so still don't have some much support."
+        }
+      }
+    }
+  ]
+}
